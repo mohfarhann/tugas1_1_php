e1857867f81796f06eb8450b2b34b1d0dcb79ff7
mohfarhann <90471536+mohfarhann@users.noreply.github.com>
2022-05-19 14:00:55 +0700

    Add files via upload

---
+++ tugas1_1.php
@@ -0,0 +1,34 @@
+<html>
+  <body>
+      <?php
+        $x1 = "Ayam Goreng";
+        $x2 = "Ayam Bakar";
+        $x3 = "Ayam Sayur";
+        $x4 = "Tempe Goreng";
+        $x5 = "Tahu Goreng";
+        $x6 = "Soto";
+        $x7 = "Nasi Putih";
+        $x8 = "Es Teh";
+        $x9 = "Es Tebu";
+
+        $y1 = 13000;
+        $y2 = 15000;
+        $y4 = 1000;
+        $y6 = 3000;
+        $y7 = 5000;
+        $y8 = 4000;
+      ?>
+      <h1>Daftar Menu</h1>
+      <ul>
+          <li><?php echo $x1 ?> = Rp. <?php echo $y1 ?></li>
+          <li><?php echo $x2 ?> = Rp. <?php echo $y2 ?></li>
+          <li><?php echo $x3 ?> = Rp. <?php echo $y1 ?></li>
+          <li><?php echo $x4 ?> = Rp. <?php echo $y4 ?></li>
+          <li><?php echo $x5 ?> = Rp. <?php echo $y4 ?></li>
+          <li><?php echo $x6 ?> = Rp. <?php echo $y6 ?></li>
+          <li><?php echo $x7 ?> = Rp. <?php echo $y7 ?></li>
+          <li><?php echo $x8 ?> = Rp. <?php echo $y8 ?></li>
+          <li><?php echo $x9 ?> = Rp. <?php echo $y7 ?></li>
+      </ul>
+  </body>
+</html>
